84a952be2e476d2fada5e60e2b2e49df8a6de4ff
Your Name <your.email@example.com>
2025-06-13 21:50:27 +0700

    change properties in be and render homepage in fe

--- at-forumfe/src/index.js
+++ at-forumfe/src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './assets/css/index.css';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/header';
-import Footer from './components/footer';
-import AppRoutes from './router/AppRoutes';
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Header />
-      <main style={{ paddingTop: '64px' }}>
-        <AppRoutes />
-      </main>
-      <Footer />
+     <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
--- at-forumfe/src/pages/Home.jsx
+++ at-forumfe/src/pages/Home.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
-import "../assets/css/home.css"
+import React, { useEffect } from "react";
+import fetchData from "../hooks/fetchData";
+import '../assets/css/home.css'
 export default function Home() {
-  return <div className="main">HOME</div>;
+  const data = fetchData("http://localhost:5000/api/thread");
+
+  useEffect(() => {
+    // Optional side-effects here
+  }, []);
+
+  return (
+    <>
+      <h1>HOME</h1>
+      <div className="item-container">
+        {data &&
+          data.map((item) => (
+            <div className="item" key={item._id}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+              <h5>{item.status}</h5>
+            </div>
+          ))}
+      </div>
+    </>
+  );
 }
