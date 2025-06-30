76e879e5f629c9a47c525018dad138ee07990983
Your Name <your.email@example.com>
2025-06-30 11:24:48 +0700

    signUp

--- at-forumfe/src/router/AppRoutes.jsx
+++ at-forumfe/src/router/AppRoutes.jsx
@@ -5,12 +5,14 @@ import Project from "../pages/Project";
 import Contact from "../pages/Contact";
 import More from "../pages/More";
 import Layout from "../components/layout";
+import Signup from "../pages/Signup"
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="" element={<Home />} />
         <Route path="/project/:keyword" element={<Project />} />
+        <Route path="/signup" element={<Signup/>}/>
         <Route path="contact" element={<Contact />} />
         <Route path="more" element={<More />} />
       </Route>
